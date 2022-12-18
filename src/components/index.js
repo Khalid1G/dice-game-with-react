ed238fea058d482c6ac895bd5348b03484785e45
Khalid1G <dev.khalid1g@gmail.com>
2022-12-18 18:41:36 +0100

    configer component

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Landing } from './landing/Landing';
+export { default as Button } from './buttons/Button';
+export { default as Dice } from './dice/Dice';
+export { default as Game } from './Game';
+
